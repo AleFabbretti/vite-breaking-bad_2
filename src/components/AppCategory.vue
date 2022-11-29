1a5c56fea5f0979ebcb6aa4509810af0ccd541e8
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-11-29 15:15:40 +0100

    Aggiunto store.js ed effettuata l'importazione nei componenti

--- src/components/AppCategory.vue
+++ src/components/AppCategory.vue
@@ -1,18 +1,24 @@
 <script>
+import { store } from "../store.js";
 export default {
-  setup() {
-    return {};
-  },
+  name: "selectedValue",
+  data() {
+    return {
+      store,
+    };
 };
 </script>
 
 <template>
   <section class="container pb-4 pl-3">
-    <select class="form-select w-auto" aria-label="Default select example">
-      <option selected>Select category</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
+    <select
+      class="form-select w-auto"
+      name="status"
+      aria-label="Default select example"
+    >
+      <option value="" selected>Select category</option>
+      <option value="Breaking Bad">Breaking Bad</option>
+      <option value="Better Call Saul">Better Call Saul</option>
     </select>
   </section>
 </template>
